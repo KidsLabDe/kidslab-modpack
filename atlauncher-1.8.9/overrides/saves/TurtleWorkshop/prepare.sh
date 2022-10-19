c045ce5ad5317aa59b1476e5b7854e7264199a49
BuildTools <gw@communitylabs.de>
2022-10-19 10:13:38 +0200

    Aufräumarbeiten und Welt als Submodule

---
+++ atlauncher-1.8.9/overrides/saves/TurtleWorkshop/prepare.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "cleaning up-"
+rm ./playerdata/*
+rm ./customnpc/playerdata/
+rm ./stats/*
+rm level.dat
+cp level-adventure.dat level.dat
+
--- create_modpack_1.8.9.sh
+++ create_modpack_1.8.9.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
+git submodule update
+cd MinecraftWorld-TurtleWorkshop/
+sh prepare.sh
+cd ..
+
 echo "generating modpack zip"
 rm modpack_1.8.9.zip
+rm -r atlauncher-1.8.9/overrides/saves/TurtleWorkshop/*
+mkdir atlauncher-1.8.9/overrides/saves/TurtleWorkshop
+cp -r MinecraftWorld-TurtleWorkshop/* atlauncher-1.8.9/overrides/saves/TurtleWorkshop/
+zip -q -r atlauncher-1.8.9/overrides/saves/TurtleWorkshop_original.zip atlauncher-1.8.9/overrides/saves/TurtleWorkshop/*
 cd atlauncher-1.8.9
-zip -r ../modpack_1.8.9.zip *    
+zip -q -r ../modpack_1.8.9.zip *    
 
 cd ..
 rm modpack_shader_1.8.9.zip
+rm -r atlauncher-shader-1.8.9/overrides/saves/TurtleWorkshop/*
+mkdir atlauncher-shader-1.8.9/overrides/saves/TurtleWorkshop
+cp -r MinecraftWorld-TurtleWorkshop/* atlauncher-shader-1.8.9/overrides/saves/TurtleWorkshop/
+zip -q -r atlauncher-shader-1.8.9/overrides/saves/TurtleWorkshop_original.zip atlauncher-shader-1.8.9/overrides/saves/TurtleWorkshop/ 
 cd atlauncher-shader-1.8.9
-zip -r ../modpack_shader_1.8.9.zip *
+zip -q -r ../modpack_shader_1.8.9.zip *
 
