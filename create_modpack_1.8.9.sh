550d3de853d427c8cc60ef577829fb5b4b1010b4
BuildTools <gw@communitylabs.de>
2022-12-31 15:51:28 +0100

    - Fehler in TurtleWorkshop Welt behoben: unterirdische Labyrinthe waren verschlossen - Fehler in create-Skript behoben

--- create_modpack_1.8.9.sh
+++ create_modpack_1.8.9.sh
@@ -6,19 +6,27 @@ cd ..
 
 echo "generating modpack zip"
 rm modpack_1.8.9.zip
+rm modpack_shader_1.8.9.zip
+
 rm -r atlauncher-1.8.9/overrides/saves/TurtleWorkshop/*
 mkdir atlauncher-1.8.9/overrides/saves/TurtleWorkshop
 cp -r MinecraftWorld-TurtleWorkshop/* atlauncher-1.8.9/overrides/saves/TurtleWorkshop/
-zip -q -r atlauncher-1.8.9/overrides/saves/TurtleWorkshop_original.zip atlauncher-1.8.9/overrides/saves/TurtleWorkshop/*
+cd atlauncher-1.8.9/overrides/saves/
+rm TurtleWorkshop_original.zip
+zip -q -r TurtleWorkshop_original.zip TurtleWorkshop/*
+cd ../../..
 cd atlauncher-1.8.9
 zip -q -r ../modpack_1.8.9.zip *    
 
 cd ..
-rm modpack_shader_1.8.9.zip
 rm -r atlauncher-shader-1.8.9/overrides/saves/TurtleWorkshop/*
 mkdir atlauncher-shader-1.8.9/overrides/saves/TurtleWorkshop
+
 cp -r MinecraftWorld-TurtleWorkshop/* atlauncher-shader-1.8.9/overrides/saves/TurtleWorkshop/
-zip -q -r atlauncher-shader-1.8.9/overrides/saves/TurtleWorkshop_original.zip atlauncher-shader-1.8.9/overrides/saves/TurtleWorkshop/ 
+cd atlauncher-shader-1.8.9/overrides/saves
+rm TurtleWorkshop_original.zip
+zip -q -r TurtleWorkshop_original.zip TurtleWorkshop
+cd ../../..
 cd atlauncher-shader-1.8.9
 zip -q -r ../modpack_shader_1.8.9.zip *
 
