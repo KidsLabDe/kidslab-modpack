7c2527d9d7d79ed1089756d8445e9036be982d53
BuildTools <gw@communitylabs.de>
2023-01-19 09:10:35 +0100

    Schildkröte vor dem ersten Haus weg...

--- create_modpack_1.8.9.sh
+++ create_modpack_1.8.9.sh
@@ -8,7 +8,7 @@ echo "generating modpack zip"
 rm modpack_1.8.9.zip
 rm modpack_shader_1.8.9.zip
 
-rm -r atlauncher-1.8.9/overrides/saves/TurtleWorkshop/*
+rm -r atlauncher-1.8.9/overrides/saves/TurtleWorkshop
 mkdir atlauncher-1.8.9/overrides/saves/TurtleWorkshop
 cp -r MinecraftWorld-TurtleWorkshop/* atlauncher-1.8.9/overrides/saves/TurtleWorkshop/
 cd atlauncher-1.8.9/overrides/saves/
@@ -19,7 +19,7 @@ cd atlauncher-1.8.9
 zip -q -r ../modpack_1.8.9.zip *    
 
 cd ..
-rm -r atlauncher-shader-1.8.9/overrides/saves/TurtleWorkshop/*
+rm -r atlauncher-shader-1.8.9/overrides/saves/TurtleWorkshop
 mkdir atlauncher-shader-1.8.9/overrides/saves/TurtleWorkshop
 
 cp -r MinecraftWorld-TurtleWorkshop/* atlauncher-shader-1.8.9/overrides/saves/TurtleWorkshop/
