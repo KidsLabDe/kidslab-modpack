0b439f21f213a539cb6113c6159dc7b604233763
BuildTools <gw@communitylabs.de>
2023-01-19 09:32:44 +0100

    Schildkröte entfernt von Haus 1

--- create_modpack_1.8.9.sh
+++ create_modpack_1.8.9.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-git submodule update
+# git submodule 
 cd MinecraftWorld-TurtleWorkshop/
+git pull
 sh prepare.sh
 cd ..
 
 echo "generating modpack zip"
-rm modpack_1.8.9.zip
-rm modpack_shader_1.8.9.zip
-
-rm -r atlauncher-1.8.9/overrides/saves/TurtleWorkshop
+rm modpack_*.zip
+	
+rm -r atlauncher-1.8.9/overrides/saves/*
 mkdir atlauncher-1.8.9/overrides/saves/TurtleWorkshop
 cp -r MinecraftWorld-TurtleWorkshop/* atlauncher-1.8.9/overrides/saves/TurtleWorkshop/
 cd atlauncher-1.8.9/overrides/saves/
@@ -25,7 +25,7 @@ mkdir atlauncher-shader-1.8.9/overrides/saves/TurtleWorkshop
 cp -r MinecraftWorld-TurtleWorkshop/* atlauncher-shader-1.8.9/overrides/saves/TurtleWorkshop/
 cd atlauncher-shader-1.8.9/overrides/saves
 rm TurtleWorkshop_original.zip
-zip -q -r TurtleWorkshop_original.zip TurtleWorkshop
+zip -q -r TurtleWorkshop_original.zip TurtleWorkshop/*
 cd ../../..
 cd atlauncher-shader-1.8.9
 zip -q -r ../modpack_shader_1.8.9.zip *
